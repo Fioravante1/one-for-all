d94509ebb81d07bcb84eec59e75afcec0b325c2c
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-09 19:31:51 -0300

    Refactor: Code climate

--- desafio8.sql
+++ desafio8.sql
@@ -1,12 +1,13 @@
 DELIMITER $$
+
+
 CREATE TRIGGER trigger_usuario_delete
 BEFORE DELETE ON usuarios
 FOR EACH ROW
 BEGIN
 DELETE FROM seguindo_artista WHERE usuario_id = OLD.usuario_id;
 DELETE FROM historico WHERE usuario_id = OLD.usuario_id;
-END
-$$
-DELIMITER;
+END $$
+DELIMITER ;
 
 
