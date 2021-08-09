62826bfdafd7a76cee00829b573ac5e8081eef05
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-09 13:54:24 -0300

    Feat: Cria a view top 3 artistas populares

---
+++ desafio4.sql
@@ -0,0 +1,14 @@
+CREATE VIEW top_3_artistas AS
+SELECT
+  a.nome_artista AS 'artista',
+  COUNT(ui.usuario_id) AS 'seguidores'
+FROM
+  artistas AS a
+  INNER JOIN seguindo_artista AS ui ON a.artista_id = ui.artista_id
+GROUP BY
+  a.artista_id
+ORDER BY
+  seguidores DESC,
+  artista
+LIMIT
+  3;
