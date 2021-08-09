fc36742d386c8f446666da2294e377ce13c7efd6
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-09 10:20:17 -0300

    Feat: Cria Tabela usuarios.

--- desafio1.sql
+++ desafio1.sql
@@ -6,3 +6,10 @@ CREATE TABLE planos(
   tipo_plano VARCHAR(50) NOT NULL,
   plano_valor DECIMAL(5, 2) NOT NULL
 ) engine = InnoDB;
+CREATE TABLE usuarios(
+  usuario_id INT PRIMARY KEY AUTO_INCREMENT,
+  usuario VARCHAR(50) NOT NULL,
+  idade INT NOT NULL,
+  plano_id INT NOT NULL,
+  FOREIGN KEY (plano_id) REFERENCES planos (plano_id)
+) engine = InnoDB;
