e26a23fc217912f6b4b1fa8cf25fc77851ada0eb
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-09 10:06:34 -0300

    Feat: Cria tabela planos

--- desafio1.sql
+++ desafio1.sql
@@ -1,2 +1,8 @@
 DROP DATABASE IF EXISTS SpotifyClone;
 CREATE DATABASE SpotifyClone;
+USE SpotifyClone;
+CREATE TABLE planos(
+  plano_id INT PRIMARY KEY AUTO_INCREMENT,
+  tipo_plano VARCHAR(50) NOT NULL,
+  plano_valor DECIMAL(5, 2) NOT NULL
+) engine = InnoDB;
